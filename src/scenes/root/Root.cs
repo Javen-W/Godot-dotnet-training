92be122a289eb626b220247b1453417aa13912a9
Javen William Zamojcin <javen.william@gmail.com>
2026-08-05 16:21:10 -0400

    add root scene

---
+++ src/scenes/root/Root.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+namespace GodotDotnetTraining
+{
+	public partial class Root : Node3D
+	{
+		private SignalConnectionManager _signals;
+
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
+			Logger.Info("Root scene ready");
+			HandleSignals();
+
+			// Emit an example signal.
+			ExampleSignalBus.EmitExample(777);
+		}
+
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		public override void _Process(double delta)
+		{
+		}
+
+		// Set up signal connections.
+		private void HandleSignals()
+		{
+			_signals = new SignalConnectionManager(this);
+
+			// Connect our OnExample() method to ExampleSignalBus.Example signal events.
+			// All handlers are automatically disconnected when the node exits the tree to avoid memory leaks.
+			_signals.Connect(
+				() => ExampleSignalBus.Instance.Example += OnExample,
+				() => ExampleSignalBus.Instance.Example -= OnExample);
+		}
+
+		private void OnExample(int value)
+		{
+			Logger.Info($"Example signal received: {value}");
+		}
+	}
+}
+
